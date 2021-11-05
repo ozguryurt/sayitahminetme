19f48aa82f3414b716550f5ca2d2f1bafc7247ca
ozguryurt <93544343+ozguryurt@users.noreply.github.com>
2021-11-05 22:37:01 +0300

    Add files via upload

---
+++ Sayı Tahmin Etme/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("[+] 0 ile 100 arasında bir sayı tahmin etme oyunu.");
+
+            Random rand = new Random();
+            int randomSayi = rand.Next(1, 100);
+
+            for(int i = 0; i<1 ; )
+            {
+                int girilenSayi = Convert.ToInt32(Console.ReadLine());
+
+                if(randomSayi < girilenSayi)
+                {
+                    Console.WriteLine("[+] Girilen sayı rastgele sayıdan daha büyük.");
+                }
+                else if (randomSayi > girilenSayi)
+                {
+                    Console.WriteLine("[+] Girilen sayı rastgele sayıdan daha küçük.");
+                }
+                else if (randomSayi == girilenSayi)
+                {
+                    Console.WriteLine("[+] Girilen sayı rastgele sayıya eşit, sayıyı buldunuz. Sayı: " + randomSayi);
+                    i++;
+                }
+            }
+
+        }
+    }
+}
